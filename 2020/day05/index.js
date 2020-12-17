0c46d731bad5fe623226831aa5ce9150d6f969f7
Bartosz Kustra <bart@webflow.com>
2020-12-18 00:54:05 +0100

    2020:05:p2

--- 2020/day05/index.js
+++ 2020/day05/index.js
@@ -4,26 +4,60 @@ console.clear()
 
 const day = '05'
 const dir = `2020/day${day}`
-const filename = `${day}.sample`
+const filename = `${day}.in`
 let input = importFile(dir, filename).replace(/\r/g, '').split('\n')
 
-let seats = new Array(0, 127)
-console.log(seats)
+const sortBySeatId = (a, b) => {
+  if (a > b) return 1
+  if (a < b) return -1
+  return 0
+}
 
 
 const part1 = () => {
+  let arr = []
   input.forEach(boardingPass => {
-    
+    let row = new Array(0, 127)
+    let col = new Array(0, 7)
+    for(let i = 0; i < boardingPass.length - 3; i++) {
+      const letter = boardingPass[i]
+      if(letter === 'F') {
+        row[1] = Math.floor(row[1] - (row[1]-row[0]) / 2)
+      } else {
+        row[0] = Math.ceil(row[0] + ((row[1]-row[0]) / 2))
+      }
+    }
+    for(let i = boardingPass.length - 3; i < boardingPass.length; i++) {
+      const letter = boardingPass[i]
+      if(letter === 'L') {
+        col[1] = Math.floor(col[1] - (col[1]-col[0]) / 2)
+      } else {
+        col[0] = Math.ceil(col[0] + ((col[1]-col[0]) / 2))
+      }
+    }
+    arr.push(row[0] * 8 + col[0])
   })
+  arr.sort(sortBySeatId)
 
-  return '\n'
+  return arr
 }
-const part2 = () => {}
+const part2 = (seatIds) => {
+  let result = 0
+  seatIds.forEach((seat, index) => {
+    if(index !== seatIds.length - 1) {
+      if(seatIds[index + 1] - seat !== 1) {
+        result = seat + 1
+      }
+    }
+  })
+  return result
+}
 
 console.time('part1')
-console.log('part1:', part1())
+const part1result = part1()
+console.log('part1:', part1result[part1result.length - 1])
 console.timeEnd('part1')
 
 console.time('part2')
-console.log('part2:', part2())
+console.log('part2:', part2(part1result))
 console.timeEnd('part2')
