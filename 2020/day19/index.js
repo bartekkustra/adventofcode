7b0e5c854fe0c79967367566236b2ec833c88ba2
Bartosz Bart Kustra <bartekkustra@gmail.com>
2020-12-20 22:52:02 +0100

    2020:19.p1

--- 2020/day19/index.js
+++ 2020/day19/index.js
@@ -4,7 +4,7 @@ console.clear()
 
 const day = '19'
 const dir = `2020/day${day}`
-const filename = `${day}.sample`
+const filename = `${day}.in`
 let [inputRules, inputLines] = importFile(dir, filename).replace(/\r/g, '').split('\n\n')
 let rawRules = new Map()
 let rulesMemo = new Map()
@@ -17,21 +17,53 @@ inputRules.forEach(rule => {
   rawRules.set(parseInt(rule[0], 10), rule[1])
 })
 
-const computeRules = (ruleKey, ruleData) => {
-  console.log(ruleKey, '->', ruleData)
-  if (rulesMemo.has(ruleKey)) return rulesMemo.get(ruleKey)
-
+const computeRules = (ruleKey) => {
+  const currentRuleData = rawRules.get(ruleKey)
   
+  if (rulesMemo.has(ruleKey)) return rulesMemo.get(ruleKey)
+  
+  let result = ''
+  if (/^".*"$/.test(currentRuleData)) {
+    result = currentRuleData.replace(/"/g, '')
+    rulesMemo.set(ruleKey, result)
+  } else if (/\|/.test(currentRuleData)) {
+    const options = currentRuleData.split(' | ')
+    result = `(${computeRules(options[0])}|${computeRules(options[1])})`
+  } else {
+    if (currentRuleData === undefined) {
+      const keys = ruleKey.split(' ')
+      keys.forEach(key => {
+        result += computeRules(parseInt(key, 10))
+      })
+    } else {
+      const keys = currentRuleData.split(' ')
+      keys.forEach(key => {
+        result += computeRules(parseInt(key, 10))
+      })
+    }
+  }
+  if(typeof ruleKey === 'number') {
+    rulesMemo.set(ruleKey, result)
+  }
+  return result
 }
 
 const part1 = () => {
-  console.log('rawRules', rawRules)
-  rawRules.forEach((rule, key)=> {
-    computeRules(key, rule)
+  rawRules.forEach((rule, key) => {
+    computeRules(key)
   })
+  
+  let ruleZero = new RegExp(`^${rulesMemo.get(0)}$`)
+
+  const validInputLines = inputLines.filter(x => ruleZero.test(x)).length
+  return validInputLines
 }
 
-const part2 = () => {}
+const part2 = () => {
+  // update rawRules
+  rawRules.set(8, '42 | 42 8')
+  rawRules.set(11, '42 31 | 42 11 31')
+}
 
 console.time('part1')
 console.log('part1:', part1())
