6e17a916d6de04c0a9e80dd66e2d570d4e81382a
Bartosz Bart Kustra <bartekkustra@gmail.com>
2020-12-20 18:01:45 +0100

    2020:18.p2

--- 2020/day18/index.js
+++ 2020/day18/index.js
@@ -7,7 +7,7 @@ const dir = `2020/day${day}`
 const filename = `${day}.in`
 let input = importFile(dir, filename).replace(/\r/g, '').split('\n')
 
-const solve = (line) => {
+const solvePart1 = (line) => {
   let tokens = line.split(' ')
   while(tokens.length > 1) {
     const result = eval(tokens.slice(0, 3).join(''))
@@ -16,11 +16,16 @@ const solve = (line) => {
   return tokens[0]
 }
 
-const solveParenthesis = (line) => {
+const solvePart2 = (line) => {
+  while(/\+/.test(line)) {
+    line = line.replace(/(\d+) \+ (\d+)/, (match, a, b) => parseInt(a, 10) + parseInt(b, 10))
+  }
+  return eval(line)
+}
+
+const solveWithParenthesis = (line, solve) => {
   while(/\(/.test(line)) {
-    line = line.replace(/\(([^()]+)\)/g, (match, equation) => {
-      return solve(equation)
-    })
+    line = line.replace(/\(([^()]+)\)/g, (match, equation) => solve(equation))
   }
   return solve(line)
 }
@@ -30,18 +35,28 @@ const part1 = () => {
   let sum = 0
   input.forEach(element => {
     if (element.indexOf('(') > -1) {
-      sum += solveParenthesis(element)
+      sum += solveWithParenthesis(element, solvePart1)
     } else {
-      sum += solve(element)
+      sum += solvePart1(element)
+    }
+  });
+  return sum
+}
+
+const part2 = () => {
+  let sum = 0
+  input.forEach(element => {
+    if (element.indexOf('(') > -1) {
+      sum += solveWithParenthesis(element, solvePart2)
+    } else {
+      sum += solvePart2(element)
     }
   });
 
   return sum
 }
-const part2 = () => {}
 
 console.time('part1')
-console.log('  expecting 71, 51, 26, 437, 12240, 13632')
 console.log('part1:', part1())
 console.timeEnd('part1')
 
