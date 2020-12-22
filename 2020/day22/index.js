cac76c7afe4d94668b9d3073a40e7d9825ef680b
Bartosz Kustra <bart@webflow.com>
2020-12-22 21:59:11 +0100

    2020:22.p2-try

--- 2020/day22/index.js
+++ 2020/day22/index.js
@@ -1,10 +1,11 @@
 import { importFile } from '../../utils/index.mjs'
+import _ from 'lodash'
 
 console.clear()
 
 const day = '22'
 const dir = `2020/day${day}`
-const filename = `${day}.in`
+const filename = `${day}.sample`
 
 let players = new Map()
 let input = 
@@ -20,12 +21,13 @@ let input =
  * @name dealCards
  * @description deals cards from the top and updates
  * 'players' map
+ * @param {Map<String, Array>} playersMap map of players and cards
  * 
  * @returns {boolean} has someone won?
  */
-const dealCards = () => {
-  let player1cards = players.get('Player 1')
-  let player2cards = players.get('Player 2')
+const dealCards = (playersMap) => {
+  let player1cards = playersMap.get('Player 1')
+  let player2cards = playersMap.get('Player 2')
   if (player1cards[0] > player2cards[0]) {
     player1cards.push(player1cards[0], player2cards[0])
   } else if (player1cards[0] < player2cards[0]) {
@@ -36,32 +38,49 @@ const dealCards = () => {
   }
   player1cards.shift()
   player2cards.shift()
-  players.set('Player 1', player1cards)
-  players.set('Player 2', player2cards)
+  playersMap.set('Player 1', player1cards)
+  playersMap.set('Player 2', player2cards)
   
-  if( player1cards.length === 0 || player2cards.length === 0) return false
+  if(player1cards.length === 0 || player2cards.length === 0) return false
   return true
 }
 
 const part1 = () => {
+  let GAME1_players = _.cloneDeep(players)
   let deal = true
   while(deal) {
-    deal = dealCards()
+    deal = dealCards(GAME1_players)
   }
 
   let winningPlayer;
-  players.forEach((cards, name) => {
+  GAME1_players.forEach((cards, name) => {
     if(cards.length !== 0) winningPlayer = name
   })
   
-  const winningCards = players.get(winningPlayer)
+  const winningCards = GAME1_players.get(winningPlayer)
   let sum = 0
   for(let i = 0; i < winningCards.length; i++) {
     sum += winningCards[i] * (winningCards.length - i)
   }
   return sum
 }
-const part2 = () => {}
+
+const part2 = () => {
+  console.log('\n\n—————————— PART 2 ——————————')
+  console.log('       expecting 291')
+  console.log('————————————————————————————\n\n')
+
+  let GAME2_players = _.cloneDeep(players)
+  console.log(GAME2_players)
+
+  let previousRounds = new Map() // Map<Number, Map<String, Array>>
+  let i = 0
+  if (previousRounds.has(i - 1)) {
+    const prev = previousRounds.get(i-1)
+    const curr = previousRounds.get(i)
+    console.log(_.isEqual(prev, curr))
+  }
+}
 
 console.time('part1')
 console.log('part1:', part1())
