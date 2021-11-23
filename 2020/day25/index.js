143927b33923856ea8d30bffb2a84a65c65ddc55
Bartosz Kustra <bartekkustra@gmail.com>
2021-11-23 23:02:39 +0100

    d25 start

---
+++ 2020/day25/index.js
@@ -0,0 +1,19 @@
+import { importFile } from '../../utils/index.mjs'
+
+console.clear()
+
+const day = '25'
+const dir = `2020/day${day}`
+const filename = `${day}.in`
+let input = importFile(dir, filename).replace(/\r/g, '').split('\n')
+
+const part1 = () => {}
+const part2 = () => {}
+
+console.time('part1')
+console.log('part1:', part1())
+console.timeEnd('part1')
+
+console.time('part2')
+console.log('part2:', part2())
+console.timeEnd('part2')
