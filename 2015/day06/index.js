07b0ec1cc0a952511b476f5efc72c33f4fd007c4
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-04 02:14:48 +0100

    2015/d7start

--- 2015/day06/index.js
+++ 2015/day06/index.js
@@ -26,7 +26,7 @@ let input = importFile(dir, filename)
 
 console.clear()
 
-const runThroughGrid = (grid, inst, from, to) => {
+const runThroughGridPart1 = (grid, inst, from, to) => {
   for (let row = from.x; row <= to.x; row++) {
     for (let col = from.y; col <= to.y; col++) {
       const pos = `${row},${col}`
@@ -50,6 +50,31 @@ const runThroughGrid = (grid, inst, from, to) => {
   return grid
 }
 
+const runThroughGridPart2 = (grid, inst, from, to) => {
+  let current = 0
+  for (let row = from.x; row <= to.x; row++) {
+    for (let col = from.y; col <= to.y; col++) {
+      const pos = `${row},${col}`
+      current = grid.has(pos) ? grid.get(pos) : 0
+      switch(inst) {
+        case 'turn on':
+          grid.set(pos, current + 1)
+          break
+        case 'turn off':
+          const newBrightness = current - 1 < 0 ? 0 : current - 1
+          grid.set(pos, newBrightness)
+          break
+        case 'toggle':
+          grid.set(pos, current + 2)
+          break
+        default:
+          throw new Error('Incorrect instruction')
+      }
+    }
+  }
+  return grid
+}
+
 const part1 = () => {
   let grid = new Set()
   /*
@@ -59,14 +84,27 @@ const part1 = () => {
     )
   */
   input.forEach(({instruction, from, to}) => {
-    grid = runThroughGrid(grid, instruction, from, to)
+    grid = runThroughGridPart1(grid, instruction, from, to)
   })
   return grid.size
 }
 
 const part2 = () => {
-
-  return 0
+  let grid = new Map()
+  /*
+    grid = Map(
+      x,y => brightness
+    )
+  */
+ input.forEach(({instruction, from, to}) => {
+   grid = runThroughGridPart2(grid, instruction, from, to)
+ })
+   
+ let sumOfBrightness = 0
+ grid.forEach(brightness => {
+   sumOfBrightness += brightness
+ })
+ return sumOfBrightness
 }
 
 const p1start = performance.now()
---
+++ 2015/day07/index.js
@@ -0,0 +1,33 @@
+import { performance } from 'perf_hooks'
+import { importFile } from '../../utils/index.mjs'
+
+const day = '06'
+const dir = `2015/day${day}`
+const filename = `${day}.in`
+let input = importFile(dir, filename)
+  .split('\r\n')
+
+console.clear()
+
+const part1 = () => {
+ 
+  return 0
+}
+
+const part2 = () => {
+ 
+  return 0
+}
+
+const p1start = performance.now()
+const p1 = part1()
+const p1end = performance.now()
+
+const p2start = performance.now()
+const p2 = part2()
+const p2end = performance.now()
+
+console.log(`part1: ${(p1end - p1start).toFixed(3)}ms`)
+console.log('part1', p1)
+console.log(`part2: ${(p2end - p2start).toFixed(3)}ms`)
+console.log('part2', p2)
