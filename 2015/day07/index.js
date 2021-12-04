a4852b100b0e8337e4c29a39c25b3a166d18407c
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-04 02:50:11 +0100

    2015d7p1 input parser

--- 2015/day07/index.js
+++ 2015/day07/index.js
@@ -1,13 +1,47 @@
 import { performance } from 'perf_hooks'
 import { importFile } from '../../utils/index.mjs'
 
-const day = '06'
+const day = '07'
 const dir = `2015/day${day}`
-const filename = `${day}.in`
+const filename = `${day}.sample`
 let input = importFile(dir, filename)
   .split('\r\n')
+  .map(op => {
+    const parsed = op.split(' ')
+    switch(parsed.length) {
+      case 3:
+        // value -> wire
+        return {
+          ops: "TRANSFER",
+          to: parsed[2],
+          value: parsed[0],
+        }
+        break
+      case 4:
+        // NOT wire -> wire
+        return {
+          ops: "NOT",
+          from: parsed[1],
+          to: parsed[3],
+        }
+        break
+      case 5:
+        // wire operation wire -> wire
+        return {
+          ops: "CALC",
+          bitwise: parsed[1],
+          left: parsed[0],
+          right: parsed[2],
+          to: parsed[4],
+        }
+        break
+      default:
+        throw new Error('Strange case I guess', parsed)
+    }
+  })
 
 console.clear()
+console.log(input)
 
 const part1 = () => {
  
