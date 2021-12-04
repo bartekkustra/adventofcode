0e96fa605701abca8e95b4bbf7a779564e6db9eb
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-04 01:59:40 +0100

    2015 d6p1

--- 2015/day06/index.js
+++ 2015/day06/index.js
@@ -1,57 +1,67 @@
-import md5 from 'md5'
+import { performance } from 'perf_hooks'
 import { importFile } from '../../utils/index.mjs'
 
 const day = '06'
 const dir = `2015/day${day}`
-const filename = `${day}.sample`
+const filename = `${day}.in`
 let input = importFile(dir, filename)
-  .replace(/\r/g, '')
-  .split('\n')
+  .split('\r\n')
+  .map(x => {
+    const [all, instruction, from, to] = x
+      .match('^(turn on|toggle|turn off) ([0-9]{1,},[0-9]{1,}) through ([0-9]{1,},[0-9]{1,})')
+    const [fromX, fromY] = from.split(',').map(Number)
+    const [toX, toY] = to.split(',').map(Number)
+    return {
+      instruction, 
+      from: {
+        x: fromX,
+        y: fromY,
+      },
+      to: {
+        x: toX,
+        y: toY,
+      }
+    }
+  })
 
 console.clear()
 
-const size = 10
-const lightsRow = new Array(size).fill(new Array(size).fill(false))
-
-const parseInstruction = (instruction) => {
-  const {decision, from, to} = instruction.match(/(?<decision>turn on|turn off|toggle) (?<from>\d,\d) through (?<to>\d,\d)/).groups
-  const [fromX, fromY] = from.split(',').map(Number)
-  const [toX, toY] = to.split(',').map(Number)
-  console.log(instruction)
-  // console.log({fromX, toX, fromY, toY})
-  for(let x = fromX; x <= toX; x++) {
-    for(let y = fromY; y <= toY; y++) {
-      switch(decision) {
+const runThroughGrid = (grid, inst, from, to) => {
+  for (let row = from.x; row <= to.x; row++) {
+    for (let col = from.y; col <= to.y; col++) {
+      const pos = `${row},${col}`
+      switch(inst) {
         case 'turn on':
-          lightsRow[x][y] = true
+          grid.add(pos)
           break
         case 'turn off':
-          lightsRow[x][y] = false
+          grid.delete(pos)
           break
         case 'toggle':
-          const lights = lightsRow[x][y]
-          console.log({x, y, lights})
-          lightsRow[x][y] = !lights
+          grid.has(pos)
+            ? grid.delete(pos)
+            : grid.add(pos)
           break
         default:
-          throw new Error(`Incorrect decision: "${decision}"`)
+          throw new Error('Incorrect instruction')
       }
     }
   }
-  console.table(lightsRow)
+  return grid
 }
 
 const part1 = () => {
-  console.log(input)
-  console.table(lightsRow)
-  
-  input.forEach(line => {
-    parseInstruction(line)
+  let grid = new Set()
+  /*
+    grid = Set(
+      x,y,
+      x2,y2
+    )
+  */
+  input.forEach(({instruction, from, to}) => {
+    grid = runThroughGrid(grid, instruction, from, to)
   })
-  
-  console.table(lightsRow)
-
-  return 0
+  return grid.size
 }
 
 const part2 = () => {
@@ -59,12 +69,15 @@ const part2 = () => {
   return 0
 }
 
-console.time('part1')
+const p1start = performance.now()
 const p1 = part1()
-console.timeEnd('part1')
-console.log(p1)
+const p1end = performance.now()
 
-console.time('part2')
+const p2start = performance.now()
 const p2 = part2()
-console.timeEnd('part2')
-console.log(p2)
+const p2end = performance.now()
+
+console.log(`part1: ${(p1end - p1start).toFixed(3)}ms`)
+console.log('part1', p1)
+console.log(`part2: ${(p2end - p2start).toFixed(3)}ms`)
+console.log('part2', p2)
