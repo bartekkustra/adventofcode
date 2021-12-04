d14e9b4d3b5d08dde045eff054a30873d068d3f1
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-04 14:56:00 +0100

    add days template

---
+++ 2021/_template/index.js
@@ -0,0 +1,32 @@
+import { performance } from 'perf_hooks'
+import { importFile } from '../../utils/index.mjs'
+
+console.clear()
+
+const day = '00'
+const dir = `2021/day${day}`
+const filename = `${day}.sample`
+let input = importFile(dir, filename)
+
+const part1 = () => {
+  
+  return 0
+}
+
+const part2 = () => {
+  
+  return 0
+}
+
+const p1start = performance.now()
+const p1 = part1()
+const p1end = performance.now()
+
+const p2start = performance.now()
+const p2 = part2()
+const p2end = performance.now()
+
+console.log(`part1: ${(p1end - p1start).toFixed(3)}ms`)
+console.log('part1', p1)
+console.log(`part2: ${(p2end - p2start).toFixed(3)}ms`)
+console.log('part2', p2)
