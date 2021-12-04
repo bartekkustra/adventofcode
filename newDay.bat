6500197a5f0d7edfb790fe9bfad8cca1008c3248
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-04 15:04:21 +0100

    d5 start

--- newDay.bat
+++ newDay.bat
@@ -2,7 +2,7 @@
 cd 2021
 mkdir day%1
 
-echo F|xcopy %CD%\_template\0.in %CD%\day%1\%1.in /q
-echo F|xcopy %CD%\_template\0.sample %CD%\day%1\%1.sample /q
+echo F|xcopy %CD%\_template\00.in %CD%\day%1\%1.in /q
+echo F|xcopy %CD%\_template\00.sample %CD%\day%1\%1.sample /q
 echo F|xcopy %CD%\_template\index.js %CD%\day%1\index.js /q
 echo F|xcopy %CD%\_template\README.md %CD%\day%1\README.md /q
