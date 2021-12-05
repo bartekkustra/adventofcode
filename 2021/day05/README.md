77b9f3a776f4835968a5f1bf34db756e04371e1c
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-12-05 16:13:49 +0100

    d5p2 clean

--- 2021/day05/README.md
+++ 2021/day05/README.md
@@ -1,3 +1,3 @@
 ## JavaScript
-#### 
-#### 
+#### part1: 30.130ms
+#### part2: 53.044ms
--- 2021/day05/index.js
+++ 2021/day05/index.js
@@ -3,17 +3,110 @@ import { importFile } from '../../utils/index.mjs'
 
 console.clear()
 
-const day = '00'
+const day = '05'
 const dir = `2021/day${day}`
-const filename = `${day}.sample`
-let input = importFile(dir, filename)
+const filename = `${day}.in`
+
+let stepsPartOne = []
+let stepsPartTwo = []
+let input = importFile(dir, filename).split('\r\n').map(x => {
+  const coords = x.match('([0-9]{1,},[0-9]{1,}) -> ([0-9]{1,},[0-9]{1,})')
+  const [fromStr, toStr] = [coords[1], coords[2]]
+  let fromArr = fromStr.split(',').map(Number)
+  let toArr = toStr.split(',').map(Number)
+
+  if (fromArr[0] === toArr[0] || fromArr[1] === toArr[1]) {
+    stepsPartOne.push({
+      from: {
+        col: fromArr[0],
+        row: fromArr[1],
+      },
+      to: {
+        col: toArr[0],
+        row: toArr[1],
+      }
+    })
+  }
+  stepsPartTwo.push({
+    from: {
+      col: fromArr[0],
+      row: fromArr[1],
+    },
+    to: {
+      col: toArr[0],
+      row: toArr[1],
+    }
+  })
+})
+
+const runThroughGrid = (g, s) => {
+  const {from, to} = s
+
+  const vectorX = (to.col - from.col) / Math.abs(to.col - from.col) || 0
+  const vectorY = (to.row - from.row) / Math.abs(to.row - from.row) || 0
+
+  let pos = {
+    x: from.col,
+    y: from.row
+  }
+  
+  while(true) {
+    const posStr = `${pos.x},${pos.y}`
+    const curr = g.get(posStr) || 0
+    g.set(posStr, curr + 1)
+
+    if (pos.x === to.col && pos.y === to.row) {
+      break
+    }
+
+    pos.x += vectorX
+    pos.y += vectorY
+  }
+
+  return g
+}
+
+const drawGrid = (g) => {
+  const GRID_SIZE = 10
+  let log = ''
+  for(let x = 0; x < GRID_SIZE; x++) {
+    for (let y = 0; y < GRID_SIZE; y++) {
+      const pos = `${x},${y}`
+      if (g.has(pos)) {
+        log += g.get(pos)
+      } else {
+        log += '.'
+      }
+    }
+    log += '\n'
+  }
+  console.log(log)
+}
 
 const part1 = () => {
-  
-  return 0
+  let grid = new Map()
+  stepsPartOne.forEach(step => {
+    grid = runThroughGrid(grid, step)
+  })
+
+  let dangerousAreas = 0
+  grid.forEach(el => {
+    if (el >= 2) dangerousAreas++
+  })
+  return dangerousAreas
 }
 
 const part2 = () => {
+  let grid = new Map()
+  stepsPartTwo.forEach(step => {
+    grid = runThroughGrid(grid, step)
+  })
+
+  let dangerousAreas = 0
+  grid.forEach(el => {
+    if (el >= 2) dangerousAreas++
+  })
+  return dangerousAreas
   
   return 0
 }
